f87e9d51b889604cb255469cbd160f0250ec3b47
partydev <admin@partyrealms.com>
2021-06-27 10:55:08 -0700

    Remove non-1.17 stuff

--- plugin/src/main/java/me/badbones69/crazycrates/api/CrazyCrates.java
+++ plugin/src/main/java/me/badbones69/crazycrates/api/CrazyCrates.java
@@ -20,19 +20,7 @@ import me.badbones69.crazycrates.multisupport.HolographicSupport;
 import me.badbones69.crazycrates.multisupport.Support;
 import me.badbones69.crazycrates.multisupport.Version;
 import me.badbones69.crazycrates.multisupport.nms.NMSSupport;
-import me.badbones69.crazycrates.multisupport.nms.v1_10_R1.NMS_v1_10_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_11_R1.NMS_v1_11_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_12_R1.NMS_v1_12_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_13_R2.NMS_v1_13_R2;
-import me.badbones69.crazycrates.multisupport.nms.v1_14_R1.NMS_v1_14_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_15_R1.NMS_v1_15_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_16_R1.NMS_v1_16_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_16_R2.NMS_v1_16_R2;
-import me.badbones69.crazycrates.multisupport.nms.v1_16_R3.NMS_v1_16_R3;
 import me.badbones69.crazycrates.multisupport.nms.v1_17_R1.NMS_v1_17_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_8_R3.NMS_v1_8_R3;
-import me.badbones69.crazycrates.multisupport.nms.v1_9_R1.NMS_v1_9_R1;
-import me.badbones69.crazycrates.multisupport.nms.v1_9_R2.NMS_v1_9_R2;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.*;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -171,45 +159,9 @@ public class CrazyCrates {
         crateLocations.clear();
         crateSchematics.clear();
         Version version = Version.getCurrentVersion();
-        useNewMaterial = version.isNewer(Version.v1_12_R1);
-        useNewSounds = version.isNewer(Version.v1_8_R3);
+        useNewMaterial = version.isNewer(Version.v1_17_R1);
+        useNewSounds = version.isNewer(Version.v1_17_R1);
         switch (version) {
-            case v1_8_R3:
-                nmsSupport = new NMS_v1_8_R3();
-                break;
-            case v1_9_R1:
-                nmsSupport = new NMS_v1_9_R1();
-                break;
-            case v1_9_R2:
-                nmsSupport = new NMS_v1_9_R2();
-                break;
-            case v1_10_R1:
-                nmsSupport = new NMS_v1_10_R1();
-                break;
-            case v1_11_R1:
-                nmsSupport = new NMS_v1_11_R1();
-                break;
-            case v1_12_R1:
-                nmsSupport = new NMS_v1_12_R1();
-                break;
-            case v1_13_R2:
-                nmsSupport = new NMS_v1_13_R2();
-                break;
-            case v1_14_R1:
-                nmsSupport = new NMS_v1_14_R1();
-                break;
-            case v1_15_R1:
-                nmsSupport = new NMS_v1_15_R1();
-                break;
-            case v1_16_R1:
-                nmsSupport = new NMS_v1_16_R1();
-                break;
-            case v1_16_R2:
-                nmsSupport = new NMS_v1_16_R2();
-                break;
-            case v1_16_R3:
-                nmsSupport = new NMS_v1_16_R3();
-                break;
             case v1_17_R1:
                 nmsSupport = new NMS_v1_17_R1();
                 break;
